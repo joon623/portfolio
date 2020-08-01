5e4995b6f410b45ed2a1aabcbca60e88e2fe9c60
jun <nnamjun94@naver.com>
2020-08-01 21:45:55 +0900

    JS: remove active form nav.
    // count는 고정이 아직 안됨

--- main.js
+++ main.js
@@ -64,6 +64,14 @@ workBtnContainer.addEventListener("click", (e) => {
   if (filter == null) {
     return;
   }
+
+  // Remove selectio from  the previous item and select the new one
+  const active = document.querySelector(".category__btn.selected");
+  active.classList.remove("selected");
+  const target =
+    e.target.nodeName === "BUTTON" ? e.target : e.target.parentNode;
+  target.classList.add("selected");
+
   projectContainer.classList.add("anim-out");
 
   setTimeout(() => {
