e95d2494b9f50d39d0ea80d77ccb0f5d8460a5d5
namjun <nnamjun94@gmail.com>
2020-10-31 18:54:32 +0900

    Refactoring

--- main.js
+++ main.js
@@ -1,98 +1,101 @@
-"use strict";
+'use strict';
 
 // Make navbar transparent when it is on the top
-const navbar = document.querySelector("#navbar");
+const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
-document.addEventListener("scroll", () => {
+document.addEventListener('scroll', () => {
   // console.log(window.scrollY);
   // console.log(`navbarHeight: ${navbarHeight}`);
   if (window.scrollY > navbarHeight) {
-    navbar.classList.add("navbar--dark");
+    navbar.classList.add('navbar--dark');
   } else {
-    navbar.classList.remove("navbar--dark");
+    navbar.classList.remove('navbar--dark');
   }
 });
+
 // Navbar toggle button for small screen
-const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
-navbarToggleBtn.addEventListener("click", () => {
-  navbarMenu.classList.toggle("open");
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
 });
+
 // Handle scrolling when tapping on the navbar menu
-const navbarMenu = document.querySelector(".navbar__menu");
-navbarMenu.addEventListener("click", (event) => {
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
   const target = event.target;
   const link = target.dataset.link;
   if (link == null) {
     return;
   }
-  navbarMenu.classList.remove("open");
+  navbarMenu.classList.remove('open');
   scrollIntoViews(link);
 });
 
 // handle click on "contact me " button on home
-const button = document.querySelector(".home__contact");
+const button = document.querySelector('.home__contact');
 
-button.addEventListener("click", (e) => {
-  scrollIntoViews("#contact");
+button.addEventListener('click', (e) => {
+  scrollIntoViews('#contact');
 });
 
 //  make home slowly fade to transparent as the window scrolls down
-const home = document.querySelector(".home__container");
+const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 
-document.addEventListener("scroll", () => {
+document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
 //  Show "arrow up" button when scrolling
-const arrowUp = document.querySelector(".arrow-up");
-document.addEventListener("scroll", () => {
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
   if (window.scrollY > homeHeight / 2) {
-    arrowUp.classList.add("visible");
+    arrowUp.classList.add('visible');
   } else {
-    arrowUp.classList.remove("visible");
+    arrowUp.classList.remove('visible');
   }
 });
 
 // Handle click on the "arrow up" button
-arrowUp.addEventListener("click", () => {
-  console.log("ssdsf");
-  scrollIntoViews("#home");
+arrowUp.addEventListener('click', () => {
+  console.log('ssdsf');
+  scrollIntoViews('#home');
 });
 
 // projects
-const workBtnContainer = document.querySelector(".work__categories");
-const projectContainer = document.querySelector(".work__projects");
-const projects = document.querySelectorAll(".project");
-workBtnContainer.addEventListener("click", (e) => {
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
   const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
   if (filter == null) {
     return;
   }
 
   // Remove selectio from  the previous item and select the new one
-  const active = document.querySelector(".category__btn.selected");
-  active.classList.remove("selected");
+  const active = document.querySelector('.category__btn.selected');
+  active.classList.remove('selected');
   const target =
-    e.target.nodeName === "BUTTON" ? e.target : e.target.parentNode;
-  target.classList.add("selected");
+    e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+  target.classList.add('selected');
 
-  projectContainer.classList.add("anim-out");
+  projectContainer.classList.add('anim-out');
 
   setTimeout(() => {
     projects.forEach((project) => {
       console.log(project.dataset.type);
-      if (filter === "*" || filter === project.dataset.type) {
-        project.classList.remove("invisible");
+      if (filter === '*' || filter === project.dataset.type) {
+        project.classList.remove('invisible');
       } else {
-        project.classList.add("invisible");
+        project.classList.add('invisible');
       }
     });
-    projectContainer.classList.remove("anim-out");
+    projectContainer.classList.remove('anim-out');
   }, 300);
 });
+
 // scroll function
 function scrollIntoViews(selector) {
   const move_button = document.querySelector(selector);
-  move_button.scrollIntoView({ behavior: "smooth" });
+  move_button.scrollIntoView({ behavior: 'smooth' });
 }
