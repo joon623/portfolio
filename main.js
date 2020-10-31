b5627df623fb27aebe4f0eca566506ef1a04c7e2
namjun <nnamjun94@gmail.com>
2020-10-31 20:22:39 +0900

    console 제거

--- main.js
+++ main.js
@@ -4,8 +4,6 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-  // console.log(window.scrollY);
-  // console.log(`navbarHeight: ${navbarHeight}`);
   if (window.scrollY > navbarHeight) {
     navbar.classList.add('navbar--dark');
   } else {
